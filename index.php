e10f30010d2ae7e23596cfdaf8318e6e2f418510
dragon <dai.yamamoto324@hotmail.com>
2017-10-03 23:32:22 +0300

    Fixed IE issue

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <!-- <script data-ionic="inject">
+  <script data-ionic="inject">
     (function(w){var i=w.Ionic=w.Ionic||{};i.version='3.6.0';i.angular='4.1.3';i.staticDir='build/';})(window);
-  </script> -->
+  </script>
   <meta charset="UTF-8">
   <title>Ionic App</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, target-densityDpi=device-dpi" />  
@@ -31,49 +31,49 @@
     }
   </script>-->
   <script type="text/javascript">
-    if (!Object.hasOwnProperty('name')) {
-      Object.defineProperty(Function.prototype, 'name', {
-        get: function() {
-          var matches = this.toString().match(/^\s*function\s*(\S*)\s*\(/);
-          var name = matches && matches.length > 1 ? matches[1] : "";
-          // For better performance only parse once, and then cache the
-          // result through a new accessor for repeated access.
-          Object.defineProperty(this, 'name', {value: name});
-          return name;
-        }
-      });
-    }
-    if ( document.attachEvent ) {
+    // if (!Object.hasOwnProperty('name')) {
+    //   Object.defineProperty(Function.prototype, 'name', {
+    //     get: function() {
+    //       var matches = this.toString().match(/^\s*function\s*(\S*)\s*\(/);
+    //       var name = matches && matches.length > 1 ? matches[1] : "";
+    //       // For better performance only parse once, and then cache the
+    //       // result through a new accessor for repeated access.
+    //       Object.defineProperty(this, 'name', {value: name});
+    //       return name;
+    //     }
+    //   });
+    // }
+    // if ( document.attachEvent ) {
 
-        // create a custom property name jqmReady and set it to 0
-        document.documentElement.jqmReady = 0;
-        // since IE8 does not allow to listen to custom events, 
-        // just listen to onpropertychange
-        document.documentElement.attachEvent("onpropertychange", function(event) {
+    //     // create a custom property name jqmReady and set it to 0
+    //     document.documentElement.jqmReady = 0;
+    //     // since IE8 does not allow to listen to custom events, 
+    //     // just listen to onpropertychange
+    //     document.documentElement.attachEvent("onpropertychange", function(event) {
 
-            // if the property changed is the custom jqmReady property
-            if (event.propertyName == "jqmReady") {
-                trigAnalytics("jqmReady");
-                alert("gotcha")
-                // remove listener, since it's only used once
-                document.documentElement.detachEvent("onpropertychange", arguments.callee);
-            }
-        });
-    }
+    //         // if the property changed is the custom jqmReady property
+    //         if (event.propertyName == "jqmReady") {
+    //             trigAnalytics("jqmReady");
+    //             alert("gotcha")
+    //             // remove listener, since it's only used once
+    //             document.documentElement.detachEvent("onpropertychange", arguments.callee);
+    //         }
+    //     });
+    // }
   </script>
 
   <link href="build/main.css" rel="stylesheet">
   <script type="text/javascript">
-    // window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-    //   heap.load("708377309");
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+      heap.load("708377309");
   </script>
 </head>
 <body>
 
   <!-- Ionic's root component and where the app will load -->
   <ion-app></ion-app>
-  <!-- <script src="https://unpkg.com/truevault@0.3.0/build/index.js"></script>             -->
-  <!-- <script src="https://www.gstatic.com/firebasejs/4.3.0/firebase.js"></script> -->
+  <script src="https://unpkg.com/truevault@0.3.0/build/index.js"></script>            
+  <script src="https://www.gstatic.com/firebasejs/4.3.0/firebase.js"></script>
   
   <!-- The polyfills js is generated during the build process -->
   <script src="build/polyfills.js"></script>
